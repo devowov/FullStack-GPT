9e94135a23392a067d394061e44c321c9b5af8c2
EUNJU HWANG <dev.owov@gmail.com>
2024-09-11 21:36:48 +0900

    upgrade reqirements[UserAgnet]

--- Assingment_17.py
+++ Assingment_17.py
@@ -1,207 +1,250 @@
 import streamlit as st
-
 from langchain.document_loaders import SitemapLoader
-from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
-from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.storage import LocalFileStore
-from urllib.parse import urlparse
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAIgit 
+from langchain.callbacks.base import BaseCallbackHandler
 
-st.set_page_config(
-    page_title = "SiteGPT"
-    , page_icon = "🖥️"
+
+answers_prompt = ChatPromptTemplate.from_template(
+    """
+    Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+                                                  
+    Then, give a score to the answer between 0 and 5.
+
+    If the answer answers the user question the score should be high, else it should be low.
+
+    Make sure to always include the answer's score even if it's 0.
+
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+
+    Question: {question}
+"""
 )
 
-st.title("SiteGPT")
 
-with st.sidebar:
-    url=None
-    api_key = st.text_input( 'Input OpenAI API keys' , type='password')
-    
-    if api_key:
-        st.success('Enter OpenAI API keys')
-        url = st.text_input(
-            label = "Write down a URL(only sitemap.xml)",
-            placeholder  ="https://developers.cloudflare.com/sitemap-0.xml"
-    
-        )
-        if url:
-            st.success('Correct URL!!')
-     
-    st.markdown("""
-    Github Repo : https://github.com/devowov/FullStack-GPT
-    """)
-    
-def parse_page(soup):
-    return (
-        str(soup.find("main").get_text())
-        .replace("\n", " ")
-        .replace("\xa0", " ")
-        .replace("Edit page   Cloudflare DashboardDiscordCommunityLearning CenterSupport Portal  Cookie Settings", "")
-    )
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
 
-@st.cache_resource(show_spinner="Scraping & Embedding...")
-def load_website(url):
-    loader = SitemapLoader(
-        url
-        , filter_urls=[
-            r"^(.*\/workers-ai\/).*",
-            r"^(.*\/vectorize\/).*",
-            r"^(.*\/ai-gateway\/).*",
-        ]
-        , parsing_function=parse_page
-    )
-    
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size = 1000
-        , chunk_overlap = 200
-    )
-    
-    docs = loader.load_and_split(text_splitter = splitter)
-    
-    embeddings = OpenAIEmbeddings(api_key=api_key)
-    vector_store = FAISS.from_documents(docs, embeddings)
-    return vector_store.as_retriever()
-
-def get_answers(input):
-    docs = input['docs']
-    question = input['question']        
-    answers_chain = answers_prompt | llm
+    answers_chain = answers_prompt | llm_for_get_answer
     return {
-        'question': question,
-        'answers': [
+        "answers": [
             {
-                'answer': answers_chain.invoke(
-                    {'question': question, 'context': doc.page_content}
+                "answer": answers_chain.invoke(
+                    {
+                        "question": question,
+                        "context": doc.page_content,
+                    }
                 ).content,
-                'source': doc.metadata['source'],
-                "date": doc.metadata['lastmod']
-            } for doc in docs
-        ]
+                "source": doc.metadata["source"],
+                "date": doc.metadata["lastmod"],
+            }
+            for doc in docs
+        ],
+        "question": question,
     }
 
-def choose_answer(input):
-    question = input['question']
-    answers = input['answers']
-    
-    choose_chain = choose_prompt | llm
-    condensed = '\n\n'.join(f"{answer['answer']}\n{answer['source']}" for answer in answers)
-    
-    return choose_chain.invoke(
-        {
-            'context': condensed,
-            'question': question
-        } 
+
+choose_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Use ONLY the following pre-existing answers to answer the user's question.
+
+            Use the answers that have the highest score (more helpful) and favor the most recent ones.
+
+            Cite sources and return the sources of the answers as they are, do not change them.
+            Choose the most informed answer among the answers with the same score.
+
+            You should always respond to the source.
+
+            Answers: {answers}
+            ---
+            Examples:
+                                                  
+            The moon is 384,400 km away.
+
+            Source: https://example.com
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+
+def choose_answer(inputs):
+    answers = inputs["answers"]
+    question = inputs["question"]
+
+    choose_chain = choose_prompt | llm_for_choose_answer
+    condensed = "\n\n".join(
+        f"{answer['answer']} \nSource:{answer['source']} \nDate:{answer['date']} \n\n"
+        for answer in answers
     )
 
-def send_message(role, message, save=True):
+    return choose_chain.invoke({"answers": condensed, "question": question})
+
+
+def parse_page(soup: BeautifulSoup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return str(soup.get_text()).replace("\n", "")
+
+
+@st.cache_data(show_spinner="Loading Website...")
+def load_website(url):
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=200
+    )
+
+    loader = SitemapLoader(
+        url,
+        # filter_urls=[],
+        filter_urls=(
+            [
+                r"https:\/\/developers.cloudflare.com/ai-gateway.*",
+                r"https:\/\/developers.cloudflare.com/vectorize.*",
+                r"https:\/\/developers.cloudflare.com/workers-ai.*",
+            ]
+        ),
+        parsing_function=parse_page,
+    )
+    # loader.requests_per_second = 1
+    ua = UserAgent()
+    loader.headers = {"User-Agent": ua.random}
+    docs = loader.load_and_split(splitter)
+
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+
+    url_copy = url[:]
+    cache_filename = url_copy.replace("/", "_")
+    cache_filename.strip()
+    cache_dir = LocalFileStore(f"./.cache/{cache_filename}/")
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings,
+        cache_dir,
+    )
+    vector_store = FAISS.from_documents(docs, cached_embeddings)
+    return vector_store.as_retriever()
+
+
+# Chat & Streaming
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+
+def send_message(message, role, save=True):
     with st.chat_message(role):
-        st.write(message)
+        st.markdown(message)
     if save:
-        save_message(role, message)
+        save_message(message, role)
+
 
 def paint_history():
-    for message in st.session_state['messages']:
-        send_message(message['role'], message['message'], save=False)
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
 
-def save_message(role, message):
-    st.session_state['messages'].append(
-            {
-                'role': role,             
-                'message': message,
-            }
-        )        
 
-answers_prompt = ChatPromptTemplate.from_messages([
-    (
-        'system',
-        '''
-            다음과 같이 context를 활용하여 질문에 대답해야합니다.
-            모르겠으면 모른다고 대답하면 됩니다. 과장없이 대답하세요.
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
+st.markdown(
+    """
+    # SiteGPT
             
-            대답에 대한 점수를 부여합니다.
-            점수는 0점에서 5점까지 부여합니다.
-            질문에 대해 적절한 답변일 수록 높은 점수를 부여합니다.
-            적절하지 않을 수록 점수는 낮아집니다.
-
-            Context: {context}
-
-            예시:
-                question: 달은 얼마나 멀리 떨어져 있나요?
-                answer: 달은 384,400 km 떨어져있습니다.
-                score: 5
-
-                question: 태양은 얼마나 멀리 떨어져 있나요?
-                answer: 모릅니다.
-                score: 0
-
-                이제 당신의 차례입니다!
-
-                question: {question}
-        '''
-    ),
-    ('human', '{question}')
-])    
-
-choose_prompt = ChatPromptTemplate.from_messages([
-    (
-        'system',
-        """
-            질문에 대한 답변은 정해진 예시로만 답변한다.
-            가장 점수가 높은 답변을 최우선으로 선택하여 답변한다.
-            출처에 대해 꼭 표기한다.
-
-            context: {context}
-        """
-    ),
-    ('human', '{question}')
-])
-
-
-if not url:
-    st.markdown(
-        """
-       Ask questions about the content of Cloudflare's documentation.
+    Ask questions about the content of a website.
             
-        The chatbot gives you answers about AI Gateway, Cloudflare Vectorize, and Workers AI.
-        
-        Enter your OpenAI API Key to ask questions.
-        """
-    )    
-    st.session_state['messages'] = []    
+    Start by writing the URL of the website on the sidebar.
+"""
+)
 
-elif not ".xml" in url:
-    st.error('please write down a Sitemap URL')
-elif url:
-    retriever = load_website(url)
 
-    send_message('ai', 'Ask anything', save=False)
-
-    paint_history()
-
-    llm = ChatOpenAI(
-    # model_name="gpt-3.5-turbo"
-    # , 
-    temperature=0.1
-    , streaming=True
+with st.sidebar:
+    openai_api_key = st.text_input("Input your OpenAI API Key")
+    url = st.text_input(
+        "Write down a URL",
+        placeholder="https://example.com",
+        value="https://developers.cloudflare.com/sitemap.xml",
+        disabled=True,
     )
+    st.markdown("---")
+    st.write("Github: https://github.com/devowov/FullStack-GPT/blob/master/Assingment_17")
 
-    question = st.chat_input()
-    if question:
-        send_message('human', question, save=True)
-        
-        chain = (
-            {
-                'docs': retriever,
-                'question': RunnablePassthrough()
-            }
-            | RunnableLambda(get_answers)
-            | RunnableLambda(choose_answer)
-        )        
-        
-        result = chain.invoke(question)
-        
-        send_message('ai', result.content, save=True)
+if url:
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("Please write down a Sitemap URL.")
+    if not openai_api_key:
+        st.error("Please input your OpenAI API Key on the sidebar")
+    else:
+        paint_history()
+        llm_for_get_answer = ChatOpenAI(
+            temperature=0.1,
+            openai_api_key=openai_api_key,
+        )
+        llm_for_choose_answer = ChatOpenAI(
+            temperature=0.1,
+            openai_api_key=openai_api_key,
+            streaming=True,
+            callbacks=[ChatCallbackHandler()],
+        )
+
+        retriever = load_website(url)
+        query = st.chat_input("Ask a question to the website.")
+        if query:
+            send_message(query, "human")
+            chain = (
+                {
+                    "docs": retriever,
+                    "question": RunnablePassthrough(),
+                }
+                | RunnableLambda(get_answers)
+                | RunnableLambda(choose_answer)
+            )
+
+            with st.chat_message("ai"):
+                chain.invoke(query)
